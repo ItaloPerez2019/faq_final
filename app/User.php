01e84406c683308cdb3d5cb77a31c7fd52c94098
italoperez <iep4@njit.edu>
2018-12-01 15:15:12 -0500

    Added question , and added test for user profile. Clean up and question test.

--- app/User.php
+++ app/User.php
@@ -44,16 +44,12 @@ class User extends Authenticatable
         'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-    public function profile()
-    {
+    public function profile(){
         return $this->hasOne('App\Profile');
+
+    }
+    public function questions(){
+        return $this->hasMany('App\Question');
+
     }
 }
